c29bf0722e1f0a7f2e1f42f5f97d121f82d15a7c
haku1217 <haruki12.17birthday@gmail.com>
2019-05-15 10:58:59 +0900

    css未設定

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 
 interface IRedioFormState {
    selected: string
-
 }
 
 class RedioForm extends React.Component<any,IRedioFormState> {
@@ -23,22 +22,22 @@ class RedioForm extends React.Component<any,IRedioFormState> {
       <h3>東京都のイベント検索</h3>
       <form>
         <div>
-          <input type="radio" name="app" checked={this.state.selected === "https://api.atnd.org/events/?keyword_or=東京都&format=json"} 
+          <input id="radio1" type="radio" name="app" checked={this.state.selected === "https://api.atnd.org/events/?keyword_or=東京都&format=json"} 
           onChange={(e) => this.setState({ selected: e.target.value })} value="https://api.atnd.org/events/?keyword_or=東京都&format=json" />
-          ATND
+          <label className="radio-atnd"htmlFor='radio1'>ATND</label>
         </div>
         <div>
-          <input type="radio" name="app" checked={this.state.selected === "https://api.doorkeeper.jp/events?q=東京都"} 
+          <input id="radio2" type="radio" name="app" checked={this.state.selected === "https://api.doorkeeper.jp/events?q=東京都"} 
           onChange={(e) => this.setState({ selected: e.target.value })} value="https://api.doorkeeper.jp/events?q=東京都" />
-          Doorkeeper
+          <label className="radio-doorkeeper" htmlFor='radio2'>Doorkeeper</label>
         </div>
         <div>
-          <input type="radio" name="app" checked={this.state.selected === "https://connpass.com/api/v1/event/?keyword_or=東京都"} 
+          <input id="radio3" type="radio" name="app" checked={this.state.selected === "https://connpass.com/api/v1/event/?keyword_or=東京都"} 
           onChange={(e) => this.setState({ selected: e.target.value })} value="https://connpass.com/api/v1/event/?keyword_or=東京都" />
-          Connpass
+          <label className="radio-connpass" htmlFor='radio3'>Connpass</label>
         </div>
         <div>
-          <EventList url={this.state.selected} onSubmit={this.handleonSubmit}/>
+          <SearchButton url={this.state.selected} onSubmit={this.handleonSubmit}/>
         </div>
       </form>
       </div>
@@ -72,7 +71,8 @@ class RedioForm extends React.Component<any,IRedioFormState> {
 }
 
 
-export class EventList extends React.Component<any, any> {
+export class SearchButton extends React.Component<any, any> {
+  
   constructor(props: any) {
     super(props);
     this.onSubmit = this.onSubmit.bind(this);
@@ -80,12 +80,19 @@ export class EventList extends React.Component<any, any> {
       eventdata: []
     });
   }
+  componentDidMount() {
+    this.onSubmit();
+  }
+
+  
   public onSubmit(){
     const url = this.props.url
+
 
     fetch(url, {mode: 'cors'})
       .then(response => response.json())
       .then(text =>{
+       
         this.setState({
           eventdata: text
         })
@@ -94,23 +101,39 @@ export class EventList extends React.Component<any, any> {
       .catch(e =>{
         console.log(e);
       })
-    
-    }
+      
+  }
   public render() {
-    const eventlists = this.state.eventdata.events.map(x=> console.log(x.event_url))
 
+    const url = this.props.url
+    let eventlist;
+    switch(url){
+      case 'https://api.atnd.org/events/?keyword_or=東京都&format=json':
+        eventlist = this.state.eventdata && this.state.eventdata.events && this.state.eventdata.events.map( events =>
+          <li key={events.event.event_url}><a href={events.event.event_url} target="_blank">{events.event.title}</a></li>)
+        break;
+      case 'https://api.doorkeeper.jp/events?q=東京都':
+        eventlist = this.state.eventdata && this.state.eventdata.map( events =>
+          <li key={events.public_url}><a href={events.event.public_url} target="_blank">{events.event.title}</a></li>)
+        break;
+      case 'https://connpass.com/api/v1/event/?keyword_or=東京都':
+        eventlist = this.state.eventdata && this.state.eventdata.events && this.state.eventdata.events.map( events =>
+          <li key={events.event_url}><a href={events.event_url} target="_blank">{events.title}</a></li>)
+        break;
+    }    
+    
     return(
       <div>
         <p>{this.props.url}</p>
         <button type="button" name="submit" onClick={this.onSubmit}>検索する</button>
         <ul>
-          <li>{eventlists}</li>
+          {eventlist}
         </ul>
       </div>
-
     );
   }
 }
+
 ReactDOM.render(
   <RedioForm />,
   document.getElementById('root')
